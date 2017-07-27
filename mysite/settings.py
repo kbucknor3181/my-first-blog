d55cb9e9cacf369d453479e0776f455163072251
Khloe Bucknor <kbucknors@gmail.com>
2017-07-27 09:38:40 -0700

    added website to allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@3i9-1$wwt-l+0a4!w@qee99%j^&fjvdir6toeg+%kp4a#*$##'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<khloerose>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'khloerose.pythonanywhere.com']
 
 
 # Application definition
